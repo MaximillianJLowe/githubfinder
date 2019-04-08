6046a6f71a9f325f74c56dd5d77b473f7fedb8f6
Maximillian Lowe <maxjlowe@gmail.com>
2019-04-07 19:03:50 -0500

    Sec 7 Lec 72 Complete

---
+++ github.js
@@ -0,0 +1,16 @@
+class Github {
+  constructor() {
+    this.client_id = 'e6b340dc44d386db9465';
+    this.client_secret = 'cff22d6c02c293ffe2fb06a2cac62e1cfd8ef80f';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profileData = await profileResponse.json();
+
+    return {
+      profile
+    }
+  }
+}
